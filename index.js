a5854d5ad452a8feb2b4aacc6be85c1f9d454ee6
eudiscava29 <eudiscava29@gmail.com>
2026-08-21 15:41:58 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1039,3 +1039,4 @@ app.get('/api/search', async (req, res) => {
 
 const PORT = process.env.PORT || 10000;
 app.listen(PORT, '0.0.0.0', () => { console.log(`🚀 CercaYa activo en puerto ${PORT}`); });
+
